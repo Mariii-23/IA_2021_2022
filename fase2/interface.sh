cc46137524fa9f831e7c2af4149a531af118376d
matildeopbravo <matildeopbravo@gmail.com>
2022-01-07 19:11:46 +0000

    Updated interface.sh to install requirements if requested

--- fase2/interface.sh
+++ fase2/interface.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 echo -e '\033[1mGaranta que as bibliotecas python termcolor e pyswip estão instaladas!\033[0m\n'
 cd interface
+read -rp "Do you wish to install termcolor and pyswip modules? " yn
+if [ "$yn" = "y" ]; then
+    pip install -r requirements.txt
+fi
 python3 main.py
